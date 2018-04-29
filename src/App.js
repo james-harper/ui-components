191352992972f9d65be1150ae8211f0fb6f6cce9
James Harper <jameswilliamharper@gmail.com>
2018-04-29 13:15:59 +0100

    eslint config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <div className="App" />
     );
   }
 }
